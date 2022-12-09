498762b3836dadb07e38b69ddc5e93d15d26fbfe
SirajAhmed7 <sirajahmedkhatri32@gmail.com>
2022-12-09 13:46:24 +0530

    Add hero cta

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,6 +1,28 @@
+import { useState } from "react";
 import Navbar from "./Navbar";
 
 export default function Hero() {
+  const [cta, setCta] = useState({});
+
+  const ctaHover = function (e) {
+    setCta({
+      backgroundColor: "transparent",
+      transform: "translatey(-100%)",
+      color: "white",
+    });
+
+    console.log(e.target);
+  };
+
+  const ctaHoverOut = function () {
+    setCta({
+      backgroundColor: "white",
+      transform: "translatey(0)",
+      color: "#000a04",
+      transition: "all 0.2s 0.2s ease, transform 0.2s ease-in-out",
+    });
+  };
+
   return (
     <section className="hero-section">
       <Navbar />
@@ -12,6 +34,17 @@ export default function Hero() {
         <div className="hero-text">
           Designed to make you look and feel your best. Outfits that match your
           personality.
+        </div>
+        <div className="hero-cta-box">
+          <a
+            onMouseEnter={ctaHover}
+            onMouseOut={ctaHoverOut}
+            href="#"
+            className="hero-cta-button"
+            style={cta}
+          >
+            Shop Now
+          </a>
         </div>
       </div>
     </section>
