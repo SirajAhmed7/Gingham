d154507c2dae21c9a4d8b6fc29a79f3ec90482f0
SirajAhmed7 <sirajahmedkhatri32@gmail.com>
2022-12-11 16:34:56 +0530

    Clean up Hero.js

--- src/Components/hero/Hero.js
+++ src/Components/hero/Hero.js
@@ -3,42 +3,6 @@ import CtaButton from '../CtaButton';
 import Navbar from './Navbar';
 
 export default function Hero() {
-  const [cta, setCta] = useState({});
-
-  const ctaHover = function (e) {
-    setCta({
-      backgroundColor: 'transparent',
-      transform: 'translatey(-100%)',
-      color: 'white',
-    });
-
-    console.log(e.target);
-  };
-
-  const ctaHoverOut = function () {
-    setCta({
-      backgroundColor: 'white',
-      transform: 'translatey(0)',
-      color: '#000a04',
-      transition: 'all 0.2s 0.2s ease, transform 0.2s ease',
-    });
-  };
-
-  const handleMouseDown = function () {
-    setCta({
-      backgroundColor: 'rgb(255,255,255, .3)',
-      transform: 'translatey(-100%)',
-      color: 'white',
-    });
-  };
-  const handleMouseUp = function () {
-    setCta({
-      backgroundColor: 'transparent',
-      transform: 'translatey(-100%)',
-      color: 'white',
-    });
-  };
-
   return (
     <section className="hero-section">
       <Navbar />
